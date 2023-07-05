71bfb6eb3d28d3acb2f388db9071743469fe98c4
IgorStrah <impirator@mail.ru>
2023-07-05 19:17:15 +0300

    new_code

--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -1,16 +1,39 @@
-// 3 high, 1 high/PWM - rotation
-// 2 high, 4 high/PWM - reverse
-// 1 high, 4 high - brake
+#include <ServoSmooth.h>
+#include <Wire.h>                                     // подключаем библиотеку для работы с шиной I2C
+#include <iarduino_I2C_connect.h>                     // подключаем библиотеку для соединения arduino по шине I2C
+#include <SoftwareSerial.h>
+#include <DFPlayerMini_Fast.h>
+DFPlayerMini_Fast myMP3;
+SoftwareSerial mySerial(A2, A3); // RX, TX
+#include <IRremote.h>
+int RECV_PIN = 11;
+int counter;
+int muveflag; // 0-stop, 1-up, 2-down.
+int engdir=0; // - driver is move. i wait pin fignal for stop
+unsigned long irvalue;
+uint32_t tmr;
+boolean flag;
+boolean start1=0;
 
-#define DEADZONE 10  // "мёртвая зона" потенциометра
 
+ServoSmooth servo;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 // пины драйвера, P1 и P4 должны быть на PWM пинах! (3, 5, 6, 9, 10, 11 для NANO и UNO)
-#define P1 3
+#define P1 5
 #define P2 4
-#define P3 5
+#define P3 7
 #define P4 6
+#define pinup 12
+#define pindown 13
 
-#define potPin 0      // сюда подключен потенциометр
+iarduino_I2C_connect I2C2;                            // объявляем переменную для работы c библиотекой iarduino_I2C_connect
+byte                 REG_Massive[2];                  // Два значения. первый байт - сколько секунд работаем, вторйо байт - что приняли тот нужный ИК код ( равез 1 или 0)
+// Если значенике первого байт больше нуля - запускается программа.
+// счётчик вычитает значени. Если значения дойдт до "0" программа останавливается с проигрышем
+// если приходит по ИК коду верное значение, программа останавливается с выигришем. передаётся второй байт "1" и обнуляется первый байт
+
+
 
 boolean switch_flag;
 int potent, duty;
@@ -27,43 +50,149 @@ void setup() {
   pinMode(P4, OUTPUT);
   digitalWrite(P4, 0);
 
-  left_min = 512 - DEADZONE;   // расчёт границ мёртвой зоны
-  right_min = 512 + DEADZONE;  // расчёт границ мёртвой зоны
+  pinMode(pinup, INPUT_PULLUP); // вход с подтяжкой
+  pinMode(pindown, INPUT_PULLUP); // вход с подтяжкой
+  irrecv.enableIRIn(); // Start the receiver
+
+
+  Wire.begin(0x02);                                   // инициируем подключение к шине I2C в качестве ведомого (slave) устройства, с указанием своего адреса на шине.
+  I2C2.begin(REG_Massive);
+
+  mySerial.begin(9600);
+  myMP3.begin(mySerial);
+  myMP3.volume(10);
+  delay(20);
+
 }
 
 void loop() {
-  potent = analogRead(potPin);                    // читаем с потенциометра
 
-  if (potent > left_min && potent < right_min) {  // если мы в "мёртвой" зоне
-    // вырубить все ключи
+
+
+
+  // для начала ждём флаг от I2C.  флаг представляет 1 битное число, сколько секунд  мы ждём ИК сигнал от палочки ( из числа генерируем шим для поднятия вверх)!
+  // включаем счётчик отсчёта!
+  // ожидаем ИК сигнала от палочки. По приходу правильного сигнала выключаем звук и паркуем мандрагору в низ до pindown. -> Отправляем на мастер флаг что атака закончена.
+  // поднимаемся вверх до срабатывания pinup и параллельно машем руками, и включаем звук на плеере ( жарим на всю)
+  // Если/Когда поднялись - шевелим руками и по чуть чуть опускаемся вниз и опять поднимаемся вверх
+  // Если по окончанию таймера не пришёл ИК код - паркуем мандрагору до pindown выключаем звук отправляем на мастер что атака не произведена
+
+
+
+  // REG_Massive[0]; // байт счётчика
+  // REG_Massive[1;] // байт флага верного ИК кода. 1 если верный. ИК на каждый вбивается индивидуально в коде.
+
+  //Проверим парковку - калибровку
+ // сначало запаркуемся. делается 1 раз при включении
+
+
+ 
+
+//    while ((digitalRead(pindown) != 0) )
+//    {
+//      drivermove(2, 130) ; // down
+//    }
+//
+//    drivermove(0, 0);
+//    muveflag=0; //stop
+//    engdir=0; //motor is stoped
+//    delay(500);
+//
+// while ((digitalRead(pinup) != 1) )
+//    {
+//      drivermove(1, 130) ; // down
+//    }
+//
+//    drivermove(0, 0);
+//    muveflag=0; //stop
+//    engdir=0; //motor is stoped
+//    delay(500);
+
+
+ if (start1==0) {
+   if (((digitalRead(pindown) == 1) && (digitalRead(pinup) == 0)) || ((digitalRead(pindown) == 1) && (digitalRead(pinup) == 1))) 
+   //паркуемся. потом будем думать. 
+  {
+    while ((digitalRead(pindown) != 0) )
+    {
+      drivermove(2, 130) ; // down
+    }
+
+    drivermove(0, 0);
+    muveflag=1; //stop
+    engdir=0; //motor is stoped
+     delay(500);
+  }
+  start1=1;
+ }
+
+
+
+// дальше процедура вверх и потом вниз. ставим флаг. если лаг = 1 то мы поднимаемся, если 2  - опускаемся.
+
+
+  if      (((muveflag==1)&& (digitalRead(pinup) == 0) &&(engdir==0)))
+  {
+   engdir=1;
+   drivermove(1, 120) ; // up
+   myMP3.play(1);
+   delay(500);
+  }
+  else if (((muveflag==2)&&(digitalRead(pindown) == 1))&&(engdir==0))
+{
+   engdir=1;
+   drivermove(2, 120) ; // down
+    delay(500);
+}
+  
+
+ 
+ // Если двигались вверхз - ждём верхнюю кнопку, если вниз - нижнюю.
+   if  (((muveflag==1) && (digitalRead(pinup) == 1)) || ((muveflag==2) && (digitalRead(pindown) == 0)))
+  {
+  engdir=0;
+  drivermove(0, 0) ; // up
+  muveflag=0; //stop
+  
+  }
+
+  
+
+
+}
+
+
+int drivermove(byte enginedirection, byte speadmove) {
+
+  //Модуль стоп. Включается всегда перед переключением что-бы не каратнуть
+
+
+
+  if (enginedirection == 0) // STOP
+  {
     digitalWrite(P1, 0);
     digitalWrite(P2, 0);
     digitalWrite(P3, 0);
     digitalWrite(P4, 0);
-    switch_flag = 1;          // разрешить переключение
-  } else if (potent > right_min) {                // если мы вышли из мёртвой зоны справа
-    if (switch_flag) {        // если разрешено переключение
-      switch_flag = 0;        // запретить переключение
-      digitalWrite(P2, 0);    // вырубить Р канальный второго плеча
-      digitalWrite(P4, 0);    // вырубить N канальный второго плеча
-      delayMicroseconds(5);   // задержечка на переключение на всякий случай
-      digitalWrite(P3, 1);    // врубить Р канальный первого плеча
-    }
-    // рассчитать скважность для N канального первого плеча
-    duty = map(potent, right_min, 1023, 0, 255);
-    analogWrite(P1, duty);    // ЖАРИТЬ ШИМ!
-
-  } else if (potent < left_min) {                 // если мы вышли из мёртвой зоны слева
-    if (switch_flag) {        // если разрешено переключение
-      switch_flag = 0;        // запретить переключение
-      digitalWrite(P3, 0);    // вырубить Р канальный первого плеча
-      digitalWrite(P1, 0);    // вырубить N канальный первого плеча
-      delayMicroseconds(5);   // задержечка на переключение на всякий случай
-      digitalWrite(P2, 1);    // врубить Р канальный второго плеча
-    }
-    // рассчитать скважность для N канального второго плеча
-    duty = map(potent, left_min, 0, 0, 255);
-    analogWrite(P4, duty);    // ЖАРИТЬ ШИМ!
+    delay(10);
   }
 
+  else if (enginedirection == 1) // UP
+  {
+    digitalWrite(P2, 0);    // вырубить Р канальный второго плеча
+    digitalWrite(P4, 0);    // вырубить N канальный второго плеча
+    delayMicroseconds(5);   // задержечка на переключение на всякий случай
+    digitalWrite(P3, 1);    // врубить Р канальный первого плеча
+    analogWrite(P1, speadmove);    // ЖАРИТЬ ШИМ!
+      delay(100);
+  }
+  else if (enginedirection == 2) // DOWN
+  {
+    digitalWrite(P3, 0);    // вырубить Р канальный первого плеча
+    digitalWrite(P1, 0);    // вырубить N канальный первого плеча
+    delayMicroseconds(5);   // задержечка на переключение на всякий случай
+    digitalWrite(P2, 1);    // врубить Р канальный второго плеча
+    analogWrite(P4, speadmove);    // ЖАРИТЬ ШИМ!
+    delay(100);
+  }
 }
---
+++ Stend_magic_test/Stend_magic_test.ino
@@ -0,0 +1,124 @@
+
+
+#include <Arduino.h>
+#include <IRremote.hpp>
+
+unsigned long code,timing_welcome;
+unsigned long newCode;
+uint32_t timer = 0;
+
+bool steptic;
+
+#include <GyverStepper.h>
+GStepper<STEPPER4WIRE> stepper(2048,8, 9, 10, 11);
+   int dir;
+void setup() {
+  // наша задача - при запуске крутить мотор в сторону до нажатия на кнопку
+  pinMode(A0, INPUT_PULLUP);  // кнопка на D12 и GND
+  IrReceiver.begin(2);
+  stepper.setMaxSpeed(500);
+  stepper.setAcceleration(500);
+ 
+  stepper.setRunMode(KEEP_SPEED);
+  stepper.setSpeedDeg(-20);   // медленно крутимся НАЗАД
+
+  // пока кнопка не нажата
+  while(digitalRead(A0)) {    
+    stepper.tick();
+
+  }
+  
+  stepper.reset();
+
+  // дальше например врубаем FOLLOW_POS
+  stepper.setRunMode(FOLLOW_POS);
+  delay(5000);
+}
+
+void loop() {
+steptic= stepper.tick();
+
+
+
+if (millis() - timing_welcome > 1000)
+{
+     if ((stepper.getTargetDeg()==stepper.getCurrentDeg())&&stepper.getTargetDeg()!=0)
+{
+  delay(1500);
+        stepper.setTargetDeg(0);
+}
+  timer = millis();
+  
+}
+
+
+    if (IrReceiver.decode()) {
+      newCode=0;
+      code=0;
+      code=IrReceiver.decodedIRData.decodedRawData;
+
+      for (int i = 0; i < 32; i++) {
+          // Extract the ith bit from the old code
+          unsigned long bit = (code >> (31 - i)) & 1;
+
+          // Set the ith bit in the new code
+          newCode |= (bit << i);
+        }
+        Serial.println(newCode);
+       
+        delay(100);
+         //IrReceiver.decodedIRData.decodedRawData();
+        IrReceiver.resume();
+
+}
+
+if (stepper.getCurrentDeg()!=0) newCode=0;
+
+ if ((newCode== 1111000001)||(newCode == 16724175)) // атака проведена верно  - сворачиваемся.
+   {
+   stepper.setTargetDeg(36);
+   newCode=0;
+   }
+  else  if ((newCode == 1111000002)||(newCode == 16718055)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*2);
+  newCode=0;
+  }
+  else  if ((newCode == 1111000003)||(newCode == 16743045)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*3);
+  newCode=0;
+  }
+else  if ((newCode == 1111000004)||(newCode == 16716015)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*4);
+   newCode=0;
+  }
+else  if ((newCode == 1111000005)||(newCode == 16726215)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*5);
+  newCode=0;
+  }
+  else  if ((newCode == 1111000006)||(newCode == 16734885)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*6);
+  newCode=0;
+  }
+  else  if ((newCode == 1111000007)||(newCode == 16728765)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*7);
+   newCode=0;
+  }
+  else  if ((newCode == 1111000008)||(newCode == 16730805)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*8);
+   newCode=0;
+  }
+  else  if ((newCode == 1111000009)||(newCode == 16732845)) // атака проведена верно  - сворачиваемся.
+  {
+  stepper.setTargetDeg(36*9);
+   newCode=0;
+  }
+
+
+}
